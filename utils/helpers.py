643f02aeb85232a33e760bd404b28217527eacb2
Bazigar ( CFC ) <hdhindihub@gmail.com>
2025-09-22 17:42:02 +0530

    Update helpers.py

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,1 +1,9 @@
+from aiogram import types
+from config import CHANNELS
 
+async def check_subs(message: types.Message, user_id: int):
+    for ch in CHANNELS:
+        member = await message.bot.get_chat_member(ch, user_id)
+        if member.status in ["left", "kicked"]:
+            return False
+    return True
