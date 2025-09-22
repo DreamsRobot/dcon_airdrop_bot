ba264c1e0ed4756e57713e47a379bcf889c824a0
Bazigar ( CFC ) <hdhindihub@gmail.com>
2025-09-22 17:38:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,11 @@
+BOT_TOKEN = "YOUR_BOT_TOKEN"
 
+# Required channels
+CHANNELS = [
+    "@DconAirdropUpdate",
+    "@DconAirdropSupport"
+]
+
+ADMIN_IDS = [123456789]  # Replace with your Telegram ID
+START_BALANCE = 100
+REF_BONUS = 50
